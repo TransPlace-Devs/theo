8fb726239c6c6867a081b20a554c7896940b74b1
Cleo <cleo.debeau@gmail.com>
2022-06-20 14:24:59 -0300

    changed .length to .size

--- handlers/MENTION_VERIFIERS.js
+++ handlers/MENTION_VERIFIERS.js
@@ -7,9 +7,7 @@ exports.run = async (client, interaction, member) => {
 
     const messages = await interaction.channel.messages.fetch()
     if(interaction.user.id == "280885861984239617") {
-        console.log(messages.length)
-        console.log(messages)
-
+        console.log(messages.size)
         return
     }
 

74a6e22358817f6c187921b441d21713011732e4
Cleo <cleo.debeau@gmail.com>
2022-06-20 13:35:23 -0300

    commented out fetch message things

--- handlers/MENTION_VERIFIERS.js
+++ handlers/MENTION_VERIFIERS.js
@@ -5,14 +5,14 @@ const {
 
 exports.run = async (client, interaction, member) => {
 
-    interaction.channel.fetchMessages().then(messages => {
-        messages.forEach((item, index)=>{
-            console.log("Mention Verifiers" + item)
-         });
-    }).catch(err => {
-        console.log('Error while getting mentions: ');
-        console.log(err);
-    });
+    // interaction.channel.fetch().then(messages => {
+    //     messages.forEach((item, index)=>{
+    //         console.log("Mention Verifiers" + item)
+    //      });
+    // }).catch(err => {
+    //     console.log('Error while getting mentions: ');
+    //     console.log(err);
+    // });
 
     if(interaction.user.id == "280885861984239617") return
 
--- slash/check.js
+++ slash/check.js
@@ -1,12 +1,12 @@
 exports.run = async (client, interaction, member) => {
-    interaction.channel.fetchMessages().then(messages => {
-        messages.forEach((item, index)=>{
-            console.log(item)
-         });
-    }).catch(err => {
-        console.log('Error while getting mentions: ');
-        console.log(err);
-    });
+    // interaction.channel.fetch().then(messages => {
+    //     messages.forEach((item, index)=>{
+    //         console.log(item)
+    //      });
+    // }).catch(err => {
+    //     console.log('Error while getting mentions: ');
+    //     console.log(err);
+    // });
 }
 
 exports.data = {}

9918dd0518cf4210f3647e4b7eda214ed0f513ad
Cleo <cleo.debeau@gmail.com>
2022-06-20 13:27:49 -0300

    fixed interacton

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,18 +1,18 @@
 module.exports = async (client, interaction) => {
 
-    if (interaction.isCommand()) {
-        const command = interaction.options._subcommand
-        const cmd = client.slash.get(command)
-        if (!cmd) return;
-        else cmd.run(client, interaction, interaction.member);
-
-    } else if (interaction.isButton()) {
+    if (interaction.isButton()) {
         const command = interaction.customId.split("|")[0]
         const cmd = client.handlers.get(command)
         if (!cmd) return;
         else cmd.run(client, interaction, interaction.member);
 
-    };
+    }else if (interaction.isCommand()) {
+        const command = interaction.options._subcommand
+        const cmd = client.slash.get(command)
+        if (!cmd) return;
+        else cmd.run(client, interaction, interaction.member);
+
+    }
 
     return
 

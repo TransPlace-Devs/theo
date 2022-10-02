1d261f3d3ef0166d3c11c20c0bf855f0a2c08f0d
elise32 <27158829+elise32@users.noreply.github.com>
2022-10-01 19:18:38 -0500

    fix archive on leave feature

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,19 +1,44 @@
-const {
-    ActionRowBuilder,
-    ButtonBuilder,
-} = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const LogManager = require('../classes/LogManager');
+const VerifyTicketHandler = require('../classes/VerifyTicketHandler');
+
+/**
+ * Check if the member who left is unverified and has a ticket that needs closing, and if so, does ticket closing
+ * operations and logs the action taken.
+ * @param {Client} client The Discord client
+ * @param {GuildMember} member The member who left the guild
+ */
+async function closeVerifyTicket(client, member) {
+    // get user
+    const { user } = member;
+    const userId = user.id;
+
+    // find the thread to archive
+    const ticketHandler = new VerifyTicketHandler(client);
+    const userTickets = await ticketHandler.findUserTickets(userId);
+    if (userTickets.size === 0) { // user has no tickets, so no further actions are necessary
+        return;
+    }
+    const ticketToArchive = ticketHandler.findMostRecentThread(userTickets);
+
+    // send archive message in thread and archive the thread
+    const embed = new EmbedBuilder().setTitle('User left - archiving thread');
+    await ticketToArchive.send({ embeds: [embed] });
+    await ticketToArchive.setArchived(true);
+
+    // make log
+    const logManager = new LogManager(client);
+    logManager.logTicketArchive({
+        ticket: ticketToArchive,
+        reason: LogManager.reasons.verify.applicantLeft,
+        user,
+        userTickets,
+    });
+}
 
 module.exports = async (client, member) => {
-    if (member.user.bot) return
-
-    // Check if they have a thread open, and if so close it.
-    
-    if(!member.guild.channel) return
-
-    const existingThread = member.guild.channel.threads.cache.find(x => {
-        if (splitUp[1] == member.user.id) return splitUp[1]
-    })
-
-    if (existingThread && !existingThread.archived) existingThread.setArchived(true, "User left guild, auto archived thread.")
-
-}
+    // check if member may be an unverified user (not a bot and no roles except @everyone role)
+    if (!member.user.bot && member.roles.cache.size === 1) {
+        closeVerifyTicket(client, member);
+    }
+};
